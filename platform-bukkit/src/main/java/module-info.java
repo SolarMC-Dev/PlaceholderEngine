ed672d947094ccfbaa13a0e32c6107af780fd0d1
A248 <theanandbeh@gmail.com>
2021-09-25 08:09:17 -0400

    Add CommandSenderTokenExtrapolator to platform-bukkit

--- platform-bukkit/src/main/java/module-info.java
+++ platform-bukkit/src/main/java/module-info.java
@@ -1,4 +1,5 @@
 import gg.solarmc.placeholder.placeholder.AdditionalTokenExtrapolator;
+import gg.solarmc.placeholder.platform.bukkit.CommandSenderTokenExtrapolator;
 import gg.solarmc.placeholder.platform.bukkit.EntityTokenExtrapolator;
 import gg.solarmc.placeholder.platform.bukkit.LivingEntityTokenExtrapolator;
 import gg.solarmc.placeholder.platform.bukkit.OfflinePlayerTokenExtrapolator;
@@ -8,7 +9,8 @@ import org.bukkit.plugin.LaunchablePlugin;
 module gg.solarmc.placeholder.platform.bukkit {
     requires gg.solarmc.placeholder;
     requires org.bukkit;
-    provides AdditionalTokenExtrapolator with EntityTokenExtrapolator,
+    provides AdditionalTokenExtrapolator with
+            CommandSenderTokenExtrapolator, EntityTokenExtrapolator,
             LivingEntityTokenExtrapolator, OfflinePlayerTokenExtrapolator;
     provides LaunchablePlugin with PlaceholderEngineBukkit;
 }
